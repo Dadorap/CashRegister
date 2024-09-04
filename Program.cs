bd264438cafad2efad113717a9af97e8edda54ba
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-09-04 14:37:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02CSharpInlämningsuppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "New Customer", "Admin", "Close The Program" };
+            DateTime currentDateTime = DateTime.Now;
+
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{currentDateTime}\nKASSA");
+
+            Console.ForegroundColor = originalColor;
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int displyNum = i == 0 ? 1 : i == 1 ? 2 : 0;
+                Console.WriteLine($"{displyNum}. {list[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
