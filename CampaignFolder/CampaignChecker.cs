de8effcc3367269cb605909c8cb1225628d2f475
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-10-27 22:43:30 +0100

    fixed Campaign checker

--- CampaignFolder/CampaignChecker.cs
+++ CampaignFolder/CampaignChecker.cs
@@ -13,19 +13,19 @@ namespace CashRegisterSystem.CampaignFolder
             var campList = new GetCampaign().GetCamp();
             var date = DateOnly.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
 
+            decimal multiDiscount = 1;
+
             foreach (var camp in campList)
             {
-                if (plu == camp.ProdPLU)
+                if (plu == camp.ProdPLU && date >= camp.StartDate && date <= camp.EndDate)
                 {
-                    if (date >= camp.StartDate && date <= camp.EndDate)
-                    {
-                        decimal discount = 1 - (camp.Discount / 100m);
-                        return discount;
-                    }
+                    decimal discount = 1 - (camp.Discount / 100m);
+
+                    multiDiscount *= discount;
                 }
             }
 
-            return 1;
+            return multiDiscount;
         }
 
     }
